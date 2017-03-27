f97c96f18ee78f08413559144b1e8563d1ae6da3
adeoluwagbemiga <adeoluwagbemiga@gmail.com>
2017-03-27 18:17:06 +0100

    Update

--- app/src/main/java/com/example/users/MainActivity.java
+++ app/src/main/java/com/example/users/MainActivity.java
@@ -99,11 +99,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+       if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
--- app/src/main/java/com/example/users/ProfileDetailActivity.java
+++ app/src/main/java/com/example/users/ProfileDetailActivity.java
@@ -1,14 +1,22 @@
 package com.example.users;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 
@@ -23,8 +31,8 @@ public class ProfileDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_detail2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         Bundle intentExtras = intent.getExtras();
@@ -42,7 +50,21 @@ public class ProfileDetailActivity extends AppCompatActivity {
         mGitUrl.setText(giturl);
         mUsername.setText(username);
         //mId.setText(idstr);
-        Picasso.with(getApplicationContext()).load(avatarstr).into(mProfileImage);
+        Picasso.with(getApplicationContext()).load(avatarstr).fit().into(mProfileImage, new Callback() {
+            @Override
+            public void onSuccess() {
+                Bitmap imageBitmap = ((BitmapDrawable)mProfileImage.getDrawable()).getBitmap();
+                RoundedBitmapDrawable imageDrawable = RoundedBitmapDrawableFactory.create(Resources.getSystem(), imageBitmap);
+                imageDrawable.setCircular(true);
+                imageDrawable.setCornerRadius(Math.max(imageBitmap.getWidth(), imageBitmap.getHeight())/2.0f);
+                mProfileImage.setImageDrawable(imageDrawable);
+            }
+
+            @Override
+            public void onError() {
+                mProfileImage.setImageResource(R.drawable.ic_menu_camera);
+            }
+        });
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,9 +77,18 @@ public class ProfileDetailActivity extends AppCompatActivity {
                 startActivity(shareintent);
             }
         });
-
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setHomeButtonEnabled(true);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
 
     }
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
 }
